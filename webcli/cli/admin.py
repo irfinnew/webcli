e9e26b67b658748b83422651b35a34fb794efdec
Marcel Moreaux <git@moreaux.nl>
2020-12-03 10:43:17 +0100

    Fix oopsie

--- webcli/cli/admin.py
+++ webcli/cli/admin.py
@@ -16,24 +16,24 @@ class CommandAdmin(admin.ModelAdmin):
 		truncated = instance.url[:64]
 		return truncated if truncated == instance.url else truncated + '...'
 	_url.short_description = "Url"
-	_url.admin_order_field = url
+	_url.admin_order_field = 'url'
 
 	def _title(self, instance):
 			return f'<strong>{instance.title}</strong>' if instance.default else instance.title
 	_title.allow_tags = True
 	_title.short_description = "Title"
-	_title.admin_order_field = title
+	_title.admin_order_field = 'title'
 
 	def _active(self, instance):
 		return instance.active
 	_active.short_description = '👁'
-	_active.admin_order_field = active
+	_active.admin_order_field = 'active'
 	_active.boolean = True
 
 	def _use_count(self, instance):
 		return instance.use_count
 	_use_count.short_description = 'Use'
-	_use_count.admin_order_field = use_count
+	_use_count.admin_order_field = 'use_count'
 
 	list_display = ('keyword', '_active', '_use_count', '_title', '_url', 'created_at', 'last_used')
 	ordering = ['-default', '-active', '-use_count']
