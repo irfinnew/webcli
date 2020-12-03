ef890c5e6f7048775d4ed45179aa904d70a08dd1
Marcel Moreaux <git@moreaux.nl>
2020-12-03 10:39:26 +0100

    More admin improvements

--- webcli/cli/admin.py
+++ webcli/cli/admin.py
@@ -12,14 +12,25 @@ def make_inactive(modeladmin, request, queryset):
 make_inactive.short_description = "Deactivate selected commands"
 
 class CommandAdmin(admin.ModelAdmin):
-	def admin_url(self, instance):
-		if len(instance.url) > 64:
-			return instance.url[:64] + '...'
-		else:
-			return instance.url
-	admin_url.short_description = "Url"
+	def _url(self, instance):
+		truncated = instance.url[:64]
+		return truncated if truncated == instance.url else truncated + '...'
+	_url.short_description = "Url"
 
-	list_display = ('keyword', 'default', 'active', 'use_count', 'title', 'admin_url', 'created_at', 'last_used')
-	ordering = ['default', 'active', 'use_count']
+	def _title(self, instance):
+			return f'<strong>{instance.title}</strong>' if instance.default else instance.title
+	_title.allow_tags = True
+	_title.short_description = "Title"
+
+	def _active(self, instance):
+		return instance.active
+	_active.short_description = '👁'
+
+	def _use_count(self, instance):
+		return instance.use_count
+	_use_count.short_description = 'Use'
+
+	list_display = ('keyword', '_active', '_use_count', '_title', '_url', 'created_at', 'last_used')
+	ordering = ['-default', '-active', '-use_count']
 	actions = [make_active, make_inactive]
 admin.site.register(Command, CommandAdmin)
